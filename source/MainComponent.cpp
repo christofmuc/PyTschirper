aed5fc476373fb206a346948f2f87431fb31eeb6
cruc <christof.ruch@gmx.de>
2019-12-14 12:35:11 +0100

    Setting up an empty repository with just the required submodules to build a midikraft UI software

---
+++ source/MainComponent.cpp
@@ -0,0 +1,29 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    // Make sure you set the size of the component after
+    // you add any child components.
+    setSize (800, 600);
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainContentComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+	auto area = getLocalBounds();	
+}
