7e9cfe14c3fd0765a8cf65fe0b04c79b16fed3d6
cruc <christof.ruch@gmx.de>
2020-05-21 17:51:06 +0200

    Fixed layer access

--- source/examples/demo_diff_layers.py
+++ source/examples/demo_diff_layers.py
@@ -1,9 +1,9 @@
-from pytschirp import Rev2, Rev2Patch
+from pytschirp import Rev2
 
 # Create an object to represent the Rev2 and run the autodetection
 r = Rev2()
 
-p = r.loadSysex(R'D:\Development\github\PyTschirper\examples\Rev2_Programs_v1.0.syx')
+p = r.loadSysex(R'D:\Development\github\PyTschirper\source\examples\Rev2_Programs_v1.0.syx')
 
 print(len(p))
 
